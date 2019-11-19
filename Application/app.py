c7fcae4dfde79ab9b81291543727f72404a4deae
Adam Durar <adamdurar@yahoo.com>
2019-11-18 20:40:57 -0600

    Nelson's Lifeline

--- Application/app.py
+++ Application/app.py
@@ -12,6 +12,7 @@ from flask import Flask, jsonify, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 import json
 import psycopg2
+from config import password
 
 app = Flask(__name__)
 
@@ -20,28 +21,52 @@ app = Flask(__name__)
 # Database Setup
 #################################################
 
-conn = psycopg2.connect(host="localhost", database="tbm",
-                        user="postgres", password="Issimo86*")
+# conn = psycopg2.connect(host="localhost", database="tbm",
+#                         user="postgres", password="Issimo86*")
 
-# engine = create_engine("postgresql://postgres:" + password + "@localhost:5432/Homework09")
+
+# engine = create_engine('postgresql://postgres:' +
+#                        password + '@localhost:5432/tbm')
+# data = engine.execute("SELECT * FROM stock_data")
+
+# for x in data:
+#     print(x)
+
+# connection=engine.connect()
+
+# engine = create_engine("postgresql://postgres:" +
+#                        password + "@localhost:5432/Homework09")
 # connection = engine.connect()
 
+
 ##################################################
-
-db = SQLAlchemy(app)
-
-# reflect an existing database into a new model
-Base = automap_base()
-# reflect the tables
-Base.prepare(db.engine, reflect=True)
-
-# Save references to each table
-Stock_Data = Base.classes.stock_data
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+@app.route('/stockdata')
+def stockdata():
+    engine = create_engine('postgresql://postgres:' +
+                           password + '@localhost:5432/tbm')
+
+    data = engine.execute(
+        "SELECT id, adj_open, adj_close, symbol, end_val FROM stock_data")
+    newdata = []
+
+    for x in data:
+        d = {
+            'id': x[0],
+            'adj_open': x[1],
+            'adj_close': x[2],
+            'symbol': x[3],
+            'end_val': x[4]
+        }
+        newdata.append(d)
+
+    return jsonify(newdata)
 
 
 if __name__ == "__main__":
